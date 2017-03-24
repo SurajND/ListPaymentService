86ed6276c3aeaedbb549d70a235651d6885aec7e
Suraj <Suraj.nd4444@gmail.com>
2017-03-24 10:06:10 +0530

    List

---
+++ ListPaymentService/Controllers/ApprovePaymentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using ListPaymentService.Models;
+using ListPaymentService.Providers;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web.Http.Cors;
+
+namespace ListPaymentService.Controllers
+{
+    public class ApprovePaymentController : Controller
+    {
+        // GET: ApprovePayment
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        public class ApproveController : ApiController
+        {
+            ApproveResponse res = null;
+            [System.Web.Http.HttpPost]
+            public async Task<IHttpActionResult> MakePayment([FromBody] ApproveRequesst payment)
+            {
+                try
+                {
+                    ApprovePayment client = new ApprovePayment();
+                    res = await client.ApprovePayments(payment);
+                }
+                catch (Exception)
+                {
+                    return StatusCode(HttpStatusCode.BadRequest);
+                }
+                return Ok(res);
+            }
+        }
+
+    }
+}
